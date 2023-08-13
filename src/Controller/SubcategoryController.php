15f0c1f1b7533eb92f56281515fb8560fae62669
rita <ritaalfy@gmail.com>
2023-08-13 02:22:16 +0200

    catetsubcatindex

---
+++ src/Controller/SubcategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SubCategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SubcategoryController extends AbstractController
+{
+    public function __construct(
+        private SubCategoryRepository $subCategoryRepository,
+
+
+
+
+)
+{
+
+}
+    #[Route('/subcategory', name: 'app_subcategory')]
+
+    public function index(): Response
+    {
+
+        return $this->render('front/pages/subcategory/index.html.twig', [
+            'subCategories' => $this->subCategoryRepository->findAll(),
+        ]);
+    }
+}
